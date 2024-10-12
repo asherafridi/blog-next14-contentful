034734cd7f9da5e71aed654403cfaf26c6452e07
asherafridi <ashirafridi.work@gmail.com>
2024-10-13 02:58:22 +0500

    service section added

--- components/Header/MobileMenu.tsx
+++ components/Header/MobileMenu.tsx
@@ -1,80 +1,30 @@
-import React, { Fragment, useState } from 'react';
-import List from "@mui/material/List";
-import ListItem from "@mui/material/List";
-import Collapse from "@mui/material/Collapse";
-import Link from 'next/link'
+import React, { useState } from 'react';
+import ListItem from "@mui/material/ListItem"; // Correct import
+import Link from 'next/link';
 
 const menus = [
-    {
-        id: 1,
-        title: 'Home',
-        link: '/home',
-    },
-    {
-        id: 2,
-        title: 'Services',
-        link: '/services',
-    },
-    {
-        id: 3,
-        title: 'Portfolio',
-        link: '/portfolio',
-    },
-    {
-        id: 4,
-        title: 'ChatZone',
-        link: '/gpt',
-    },
-    {
-        id: 5,
-        title: 'Meet Now',
-        link: '/meet',
-    },
-
-
-]
+    { id: 1, title: 'Home', link: '/home' },
+    { id: 2, title: 'Services', link: '/services' },
+    { id: 3, title: 'Portfolio', link: '/portfolio' },
+    { id: 4, title: 'ChatZone', link: '/gpt' },
+    { id: 5, title: 'Meet Now', link: '/meet' },
+];
 
 const MobileMenu = () => {
-
     const [openId, setOpenId] = useState(0);
-
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
 
     return (
         <ul className="main_menu_list clearfix">
-            {menus.map((item, mn) => {
-                return (
-                <ListItem className={item.id === openId ? 'active' : undefined} key={mn}>
-                        {item.submenu ?
-                            <Fragment>
-                                <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>{item.title}
-                                    <i className={item.id === openId ? 'fa fa-angle-up' : 'fa fa-angle-down'}></i>
-                                </p>
-                                <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
-                                    <List className="subMenu">
-                                        <Fragment>
-                                            {item.submenu.map((submenu, i) => {
-                                                return (
-                                                    <ListItem key={i}>
-                                                        <Link onClick={ClickHandler} className="active"
-                                                            href={submenu.link}>{submenu.title}</Link>
-                                                    </ListItem>
-                                                )
-                                            })}
-                                        </Fragment>
-                                    </List>
-                                </Collapse>
-                            </Fragment>
-                            : <Link className="active"
-                                href={item.link}>{item.title}</Link>
-                        }
-                    </ListItem>
-                )
-            })}
+            {menus.map((item) => (
+                <ListItem className={item.id === openId ? 'active' : undefined} key={item.id}>
+                    
+                        <Link href={item.link} onClick={() => window.scrollTo(0, 0)} className="active">
+                            {item.title}
+                        </Link>
+                </ListItem>
+            ))}
         </ul>
-    )
-}
+    );
+};
 
 export default MobileMenu;
