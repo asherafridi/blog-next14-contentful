a007cb561693bb55d8c0d00a05167cc8f648a0ab
asherafridi <ashirafridi.work@gmail.com>
2024-10-15 00:48:22 +0500

    revision 1

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,36 +1,34 @@
+import dynamic from "next/dynamic";
 
-import About from "@/components/Homepage/About";
-import BlogSection from "@/components/Homepage/Blog";
-import FaqSection from "@/components/Homepage/Faq";
-import Hero from "@/components/Homepage/Hero";
-// import PartnerSection from "@/components/Homepage/Partners";
-// import ProjectSection from "@/components/Homepage/Portfolio";
-import ServiceSection from "@/components/Homepage/Services";
-import TechnologySection from "@/components/Homepage/Technology";
-// import Testimonial from "@/components/Homepage/Testimonial";
-// import dynamic from "next/dynamic";
-
-// const PartnerSection = dynamic(() => import('@/components/Homepage/Partners'), { ssr: true });
+const Hero = dynamic(() => import("@/components/Homepage/Hero"), { ssr: false });
+const About = dynamic(() => import("@/components/Homepage/About"), { ssr: false });
+const ServiceSection = dynamic(() => import("@/components/Homepage/Services"), { ssr: false });
+const TechnologySection = dynamic(() => import("@/components/Homepage/Technology"), { ssr: false });
+const FaqSection = dynamic(() => import("@/components/Homepage/Faq"), { ssr: false });
+const BlogSection = dynamic(() => import("@/components/Homepage/Blog"), { ssr: false });
+// Uncomment other sections as needed
+const Testimonial = dynamic(() => import("@/components/Homepage/Testimonial"), { ssr: false });
+const PartnerSection = dynamic(() => import("@/components/Homepage/Partners"), { ssr: false });
+const ProjectSection = dynamic(() => import("@/components/Homepage/Portfolio"), { ssr: false });
 
 import "swiper/css";
 import "swiper/css/autoplay";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-// import Loading from "../loading";
 
 export default function Home() {
   return (
     <>
-    <Hero />
-    <About />
-    <ServiceSection />
-    <TechnologySection />
-    {/* <Testimonial /> */}
-    <FaqSection />
-    {/* <ProjectSection /> */}
-    <BlogSection />
-    {/* <PartnerSection /> */}
-     </>
+      <Hero />
+      <About />
+      <ServiceSection />
+      <TechnologySection />
+      <Testimonial />
+      <FaqSection />
+      <ProjectSection />
+      <BlogSection />
+      <PartnerSection />
+    </>
   );
 }
