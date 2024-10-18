5e7f73994588cf16e7a2e2b9232d47e10b997e55
asherafridi <ashirafridi.work@gmail.com>
2024-10-18 16:13:26 +0500

    chatzone name feature

--- app/gpt/page.tsx
+++ app/gpt/page.tsx
@@ -118,7 +118,7 @@ const ChatComponent = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://3.106.186.34:85/chat", {
+      const response = await fetch("https://chat.vetaai.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
